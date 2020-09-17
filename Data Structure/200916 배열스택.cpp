9f0ca4326c8b05f930a3e4a1c41367eba4b05985
김예진 <wlsdong0004@gmail.com>
2020-09-17 10:15:13 +0900

    array stack pop, isFull 추가
    2020 09 17

--- Data Structure/200916 배열스택.cpp
+++ Data Structure/200916 배열스택.cpp
@@ -1,7 +1,8 @@
-/*스택
+/*
+//스택
  후입선출(Last In First Out)
  top이라고 하는 한쪽 끝에서 데이터의 삽입과 삭제가 일어남
- 배열 스택, 연결리스트 스택이 있다. 
+ 배열 스택, 연결리스트 스택이 있음. 
  
  // 배열 스택
  배열로 구현된 스택
@@ -16,11 +17,11 @@ int top=-1;
 
 int main(){
 	int stack[10]= {0};
-	//pop(stack);
-	for(int i=0; i<10; i++) push(stack, i);
+	pop(stack);
+	for(int i=0; i<11; i++) push(stack, i);
 	for(int i=0; i<10; i++) printf("%d\n", stack[i]);
 	printf("top: %d\n", top);
-	//pop(stack);
+	pop(stack);
 	for(int i=0; i<10; i++) printf("%d\n", stack[i]);
 	printf("top %d", top);
 }
@@ -33,4 +34,14 @@ int push(int *stack, int value){
 int isFull(){
 	if(top>=9) return 1;
 	else return 0;
+}
+
+int pop(int *stack){
+	if(isEmpty()) return printf("Stack is Empty!\n");
+	else int temp=stack[top--];
+}
+
+int isEmpty(){
+	if(top<=0) return 1;
+		else return 0; 
 }
